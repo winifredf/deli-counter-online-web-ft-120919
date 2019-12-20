25050a81c398665301af7572fc6b7209b0264eb9
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 00:23:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,17 @@
+def line(array) # this was the one I figured out
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
 katz_deli = []
 
 def take_a_number(array, name)
