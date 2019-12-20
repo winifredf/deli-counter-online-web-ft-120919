334b20ebc5a953c79566c573f5902672992d8d0f
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 00:25:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,7 @@ def line(array) # this was the one I figured out
   end
 end
 
-katz_deli = []
-
+line(katz_deli)
 def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
