3f5db1dbcade73c0d86803d8c9674f27440722b8
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 00:21:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+end
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    phrase = "The line is currently:"
+end
